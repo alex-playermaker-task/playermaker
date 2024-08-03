7140c784119b22e8e9387194c9c380bde84fb7a4
naje1 <152160512+naje1@users.noreply.github.com>
2024-08-04 01:00:24 +0300

    Initial commit with main project structure

--- src/main/java/playermaker/com/playermakertask/PlayermakerTaskApplication.java
+++ src/main/java/playermaker/com/playermakertask/PlayermakerTaskApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PlayermakerTaskApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PlayermakerTaskApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PlayermakerTaskApplication.class, args);
+    }
 
 }
--- src/test/java/playermaker/com/playermakertask/PlayermakerTaskApplicationTests.java
+++ src/test/java/playermaker/com/playermakertask/PlayermakerTaskApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PlayermakerTaskApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
