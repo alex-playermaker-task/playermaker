a1079ec339e1fc6949d255374b61fcda9040815f
naje1 <152160512+naje1@users.noreply.github.com>
2024-08-04 00:54:47 +0300

    Initial commit

---
+++ src/main/java/playermaker/com/playermakertask/PlayermakerTaskApplication.java
@@ -0,0 +1,13 @@
+package playermaker.com.playermakertask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayermakerTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayermakerTaskApplication.class, args);
+	}
+
+}
